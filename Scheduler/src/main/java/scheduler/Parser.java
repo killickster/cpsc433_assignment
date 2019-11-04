2fb80891e6aa151fb0317f67df174e3be986ca45
Micah Killick <killickster@gmail.com>
2019-11-03 23:25:01 -0700

    Parser can identify type of input

--- Scheduler/src/main/java/scheduler/Parser.java
+++ Scheduler/src/main/java/scheduler/Parser.java
@@ -29,7 +29,31 @@ public class Parser {
 
             switch(trimmedLine){
                 case "Course slots:":
-                    parseCourseSlot(reader);
+                    parseCourseSlots(reader);
+                    break;
+                case "Lab slots:":
+                    parseLabSlots(reader);
+                    break;
+                case "Courses:":
+                    parseCourses(reader);
+                    break;
+                case "Labs:":
+                    parseLabs(reader);
+                    break;
+                case "Not compatible:":
+                    parseNotCompatible(reader);
+                    break;
+                case "Unwanted:":
+                    parseUnwanted(reader);
+                    break;
+                case "Preferences:":
+                    parsePreferences(reader);
+                    break;
+                case "Pair:":
+                    parsePairs(reader);
+                    break;
+                case "Partial assignments:":
+                    parsePartialAssignments(reader);
                     break;
                 default:
                     System.out.println(trimmedLine);
@@ -38,7 +62,7 @@ public class Parser {
 
     }
 
-    public void parseCourseSlot(BufferedReader reader) throws IOException{
+    public void parseCourseSlots(BufferedReader reader) throws IOException{
 
         String line;
 
@@ -46,13 +70,164 @@ public class Parser {
 
             String trimmedLine = line.trim();
 
+            //If the line is empty 
             if(trimmedLine.length() == 0){ 
                 return;
             }
 
-            System.out.println("Course Slots: " + trimmedLine);
+            System.out.println("Course Slot: " + trimmedLine);
 
 
+
+        }
+    }
+
+    public void parseLabSlots(BufferedReader reader) throws IOException{
+    
+        String line;
+
+        while((line = reader.readLine()) != null){
+
+            String trimmedLine = line.trim();
+
+            //If the line is empty 
+            if(trimmedLine.length() == 0){ 
+                return;
+            }
+
+            System.out.println("Lab Slot: " + trimmedLine);
+
+        }
+    }
+
+
+    public void parseCourses(BufferedReader reader) throws IOException{
+    
+        String line;
+
+        while((line = reader.readLine()) != null){
+
+            String trimmedLine = line.trim();
+
+            //If the line is empty 
+            if(trimmedLine.length() == 0){ 
+                return;
+            }
+
+            System.out.println("Course: " + trimmedLine);
+
+        }
+    }
+
+
+
+    public void parseLabs(BufferedReader reader) throws IOException{
+    
+        String line;
+
+        while((line = reader.readLine()) != null){
+
+            String trimmedLine = line.trim();
+
+            //If the line is empty 
+            if(trimmedLine.length() == 0){ 
+                return;
+            }
+
+            System.out.println("Lab: " + trimmedLine);
+
+        }
+    }
+
+    public void parseNotCompatible(BufferedReader reader) throws IOException{
+    
+        String line;
+
+        while((line = reader.readLine()) != null){
+
+            String trimmedLine = line.trim();
+
+            //If the line is empty 
+            if(trimmedLine.length() == 0){ 
+                return;
+            }
+
+            System.out.println("Not Compatible: " + trimmedLine);
+
+        }
+    }
+
+    public void parseUnwanted(BufferedReader reader) throws IOException{
+    
+        String line;
+
+        while((line = reader.readLine()) != null){
+
+            String trimmedLine = line.trim();
+
+            //If the line is empty 
+            if(trimmedLine.length() == 0){ 
+                return;
+            }
+
+            System.out.println("Unwanted: " + trimmedLine);
+
+        }
+    }
+
+    public void parsePreferences(BufferedReader reader) throws IOException{
+    
+        String line;
+
+        while((line = reader.readLine()) != null){
+
+            String trimmedLine = line.trim();
+
+            //If the line is empty 
+            if(trimmedLine.length() == 0){ 
+                return;
+            }
+
+            System.out.println("Preference: " + trimmedLine);
+
+        }
+    }
+
+
+
+    public void parsePairs(BufferedReader reader) throws IOException{
+    
+        String line;
+
+        while((line = reader.readLine()) != null){
+
+            String trimmedLine = line.trim();
+
+            //If the line is empty 
+            if(trimmedLine.length() == 0){ 
+                return;
+            }
+
+            System.out.println("Pair: " + trimmedLine);
+
+        }
+    }
+
+
+    public void parsePartialAssignments(BufferedReader reader) throws IOException{
+    
+        String line;
+
+        while((line = reader.readLine()) != null){
+
+            String trimmedLine = line.trim();
+
+            //If the line is empty 
+            if(trimmedLine.length() == 0){ 
+                return;
+            }
+
+            System.out.println("Partial Assignment: " + trimmedLine);
 
         }
     }
